0854a51536498164ff22562fda8857532fe844b1
nlg550 <38725499+nlg550@users.noreply.github.com>
2018-11-28 08:40:03 +0000

    ...

--- CodeAnalyzer.cpp
+++ CodeAnalyzer.cpp
@@ -488,6 +488,7 @@ void CodeAnalyzer::analyzeFunction(Function *func, std::stack<Function*> &stack_
 	while (func->current_inst < func->Ninstructions) //iterate through all instructions specified in <func>
 	{
 		current_inst = func->instructions[func->current_inst];
+		reg.addRegister(std::stoull(current_inst.address, nullptr, 16), "rsi");
 
 		if (!leave)
 		{
@@ -831,22 +832,14 @@ void CodeAnalyzer::analyzeFunction(Function *func, std::stack<Function*> &stack_
  */
 void CodeAnalyzer::analyze()
 {
-	Function *current_func;
 	std::stack<Function*> func_stack;
 
 	func_stack.emplace(&functions["main"]);
 	allocFunction(functions["main"], 0);
-	current_func = func_stack.top();
 
 	while (!func_stack.empty())
 	{
-		if (current_func != func_stack.top())
-		{
-			current_func = func_stack.top();
-		}
-
-		analyzeFunction(current_func, func_stack);
+		analyzeFunction(func_stack.top(), func_stack);
 	}
-
 }
 
